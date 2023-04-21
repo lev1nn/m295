08146603d58e2f02ba30241849092cec41631108
lev1nn <levinsch@bluewin.ch>
2023-04-21 09:21:08 +0200

    Initial commit

---
+++ ebanking/src/test/java/ch/ilv/ebanking/EbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ilv.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
