29ea1e7bc37582305b7be718dc253cbab0acb4d7
lev1nn <levinsch@bluewin.ch>
2023-04-27 10:36:35 +0200

    Add files

---
+++ ebanking/src/main/java/ch/ilv/ebanking/base/MessageResponse.java
@@ -0,0 +1,14 @@
+package ch.ilv.ebanking.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
