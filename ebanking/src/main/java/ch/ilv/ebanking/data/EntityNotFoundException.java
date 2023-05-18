46aedc0f128fa390d49f6994f91c483ab9084080
Yannick Müller <mail@ynnk.dev>
2023-05-18 14:05:06 +0200

    removed useless classes

--- ebanking/src/main/java/ch/ilv/ebanking/data/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ch.ilv.ebanking.data;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(Long id, Class c) {
-        super("Could not find " + c.getSimpleName() + " " + id);
-    }
-}
